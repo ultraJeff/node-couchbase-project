001c8bd19719fb140fbc20b4050599d704ea9c2f
Jeff Franklin <jefffranklin@jeffs-mbp.lan>
2022-03-03 19:27:20 -0500

    hopefully final edits

--- index.js
+++ index.js
@@ -5,37 +5,45 @@ const express = require("express");
 const app = express();
 
 const endpoint = 'couchbase://cb-example';
+console.log('Connecting to: ', endpoint);
 
 async function main() {
-  console.log('trying to connect to', endpoint);
   const cluster = await couchbase.connect(`${endpoint}`, {
     username: 'Administrator',
     password: 'password',
   })
 
-  const result = await cluster.ping();
-  console.log('did we ping it', result);
+  const pingResults = await cluster.ping();
+  console.log('Couchbase up and running', pingResults);
 
   const getVaccinations = async () => {
     try {
+      // const result = 'hello'
       const result = await cluster.query(`
       SELECT * FROM \`tweets\`
       `)
-      console.log('Get Result: ')
-      console.log(result)
+      console.log('Get Results: ', result)
+      return result;
     } catch (error) {
       console.error(error)
     }
   }
 
-  app.get("/", (req, res) => {
+  app.get("/", runAsync(async (req, res) => {
     let data = await getVaccinations();
     res.send(data)
-  })
+  }))
 
   app.listen(8080, () => {
     console.log(`Server is up and running on 8080 ...`)
   });
+}
+
+function runAsync (callback) {
+  return function (req, res, next) {
+    callback(req, res, next)
+      .catch(next)
+  }
 }
 
 main();
